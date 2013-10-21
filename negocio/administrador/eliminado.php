ce2ca626b637c26e7c0fb9e1576cfa4214077f06
informix <informix@nmedios.vm.edu>
2013-10-21 18:55:04 -0200

    Marco, Profesor y Richard 21_10_2013

--- negocio/administrador/eliminado.php
+++ negocio/administrador/eliminado.php
@@ -5,8 +5,9 @@
         include "../../persistencia/Administrador.php";
         $admin=new Administrador();
         extract($_POST);
+    
         $resultado=$admin->eliminar($documento);
-        header("Location: ../../presentacion/paginas/administrador/index.php?pag=b_s")
+       header("Location: ../../presentacion/paginas/administrador/index.php?pag=b_s")
 
 
 
