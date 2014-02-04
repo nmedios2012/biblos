818488266bc61ca5c931cf73b4d21f1e407a382f
root <root@nmedios.vm.edu>
2014-02-03 23:45:50 -0200

    Freddy V_2_2_14

---
+++ presentacion/paginas/bibliotecologo/prestamo.php
@@ -0,0 +1,8 @@
+<p>ADMINISTRACI&Oacute;N DE MATERIALES - PR&Eacute;STAMOS</p>
+                <form name="input" action="../../../negocio/bibliotecologo/alta_conservacion.php" method="post">
+                <p>Documento <input type="text" name="documento" size="11" maxlength="11"></p></br>  
+                <p>C&oacute;digo Material <input type="text" name="cod_mat" size="10" maxlength="10">
+                <p>C&oacute;digo Ejemplar <input type="text" name="codigo_ejem" size="10" maxlength="10">
+                    
+                
+                </form>
