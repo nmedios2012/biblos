45ad2944755ac65e32691961bbc1d9f9d526e203
informix <informix@nmedios.vm.edu>
2014-07-05 19:13:46 -0300

    Titulo

--- presentacion/paginas/bibliotecologo/index.php
+++ presentacion/paginas/bibliotecologo/index.php
@@ -11,50 +11,55 @@ if ($_SESSION["tipo"] != "bibliotecologo")
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Documento sin título</title>
+        <title>Biblos Administracion</title>
         <link 
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
         <link href="../../css/admin.css" rel="stylesheet" type="text/css">
         <script type="text/javascript" src="../../js/jquery.js"></script>
         <script type="text/javascript" src="../../js/bibliotecologo.js"></script>
+        <script>
+            function startTime() {
+                var today = new Date();
+                var h = today.getHours();
+                var m = today.getMinutes();
+                var s = today.getSeconds();
+                m = checkTime(m);
+                s = checkTime(s);
+                document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+                var t = setTimeout(function() {
+                    startTime()
+                }, 500);
+            }
 
+            function checkTime(i) {
+                if (i < 10) {
+                    i = "0" + i
+                }
+                ;  // add zero in front of numbers < 10
+                return i;
+            }
+        </script>
     </head>
 
-    <body>
+    <body onload="startTime()">
 
         <div class="container">
             <header>
-                <a href="#"><img src="../../../presentacion/imagenes/logo.jpg" alt="" width="180" height="90" id="Insert_logo" style="background-color: #C6D580; display:block;" /></a>
-            </header><!--
-            <div class="sidebar1">
-              <ul class="nav">
-                  <li><a href="index.php?pag=a_s">Alta socio</a></li>
-                  <li><a href="../../../negocio/bibliotecologo/listadoUsuario.php">Listar socios</a></li>
-                  <li><a href="index.php?pag=b_s">Baja socio</a></li>
-                  <li><a href="../../../negocio/bibliotecologo/altaprestamo.php">Pr&eacute;stamo</a></li>
-                  <li><a href="index.php?pag=penalizaciones">Sanciones</a></li>
-                  <li><a href="index.php?pag=a_l">Alta libros</a></li>
-                  <li><a href="index.php?pag=a_r">Alta revistas</a></li>
-                  <li><a href="index.php?pag=a_f">Alta fotocopias</a></li>
-                  <li><a href="index.php?pag=a_o">Alta otros</a></li>
-                  <li><a href="index.php?pag=a_t_e">Tema/Especializaci&oacute;n</a></li>
-                  <li><a href="index.php?pag=a_c">Conservaci&oacute;n</a></li>
-                  <li><a href="index.php?pag=estado">Estado de material</a></li>
-                  <li><a href="index.php?pag=a_e">Alta editorial/empresa</a></li>
-                  <li><a href="index.php?pag=alta_autores">Artista/Autor</a></li>
-                  <li><a href="index.php?pag=a_p">Ingreso de Pa&iacute;s</a></li>
-                    
-                  <li><a href="../../../negocio/desloguearse.php">Cerrar session</a></li>
-                  <li><a href="#">Otro vínculo</a></li>
-              </ul>
-              <aside>
-                <p> </p>
-              </aside>
-            </div>-->
+                <a href="#">
+                    <img src="../../../presentacion/imagenes/logo.jpg" alt="" width="180" height="90" id="Insert_logo" style="background-color: #FFF580; display:block;" />
+                </a>                
+                <?php
+                if ($_SESSION['tipo']) { //Si hay un usuario lo mostramos
+                    echo" <strong>USUARIO: " . $_SESSION['usuario'] . "<BR/>   ROL: " . $_SESSION['tipo'] . "</strong><br />";
+                }
+                ?>
+                <div id="clock" align="right"/>
+
+            </header>
             <div id='flyout_menu' class="sidebar1">
                 <ul class="nav">
-                  
+
                     <li><a href='#'><span>Socios</span></a>
                         <ul>
                             <li>
@@ -77,7 +82,7 @@ if ($_SESSION["tipo"] != "bibliotecologo")
                     <li><a href='#'><span>Administracion</span></a>
                         <ul>
                             <li><a href='#'><span>Biblioteca</span></a>
-                                <li>
+                            <li>
                                 <a href="../../../negocio/bibliotecologo/listadoReserva.php">Listar Reservas</a>
                             </li>
                             <li><a href="index.php?pag=a_e">Alta editorial/empresa</a></li>
@@ -96,16 +101,6 @@ if ($_SESSION["tipo"] != "bibliotecologo")
                             <li><a href="index.php?pag=estado">Estado de material</a></li>
                         </ul>
                     </li>
-
-
-
-
-
-
-
-
-
-
                     <li><a href="../../../negocio/desloguearse.php">Cerrar session</a></li>
 
                 </ul>
@@ -122,14 +117,14 @@ if ($_SESSION["tipo"] != "bibliotecologo")
                 ?>
 
             </article>
-            <aside>
-                <h4>Lateral </h4>
-                <p></p>
-            </aside>
             <footer>
                 <p></p>
                 <address>
                     Contenido de dirección
+                    <?php
+                    print '<pre>' . htmlspecialchars(print_r(get_defined_vars(), true)) . '</pre>';
+                    print '<pre>' . htmlspecialchars(print_r($_SERVER, true)) . '</pre>';
+                    ?>
                 </address>
             </footer>
             <!-- end .container --></div>
